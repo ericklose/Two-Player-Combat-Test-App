42de9915da9ae2c0c79b02c0142f925dc40000f3
Eric Klose <ericklose@gmail.com>
2016-02-09 16:39:28 -0700

    Basically done with storyboard and starting on code

--- Two Player Combat Test App/ViewController.swift
+++ Two Player Combat Test App/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var leftAttackBtn: UIButton!
+    @IBOutlet weak var leftAttackLbl: UILabel!
+    @IBOutlet weak var rightAttackBtn: UIButton!
+    @IBOutlet weak var rightAttackLbl: UILabel!
+    
+    @IBOutlet weak var mainTextLbl: UILabel!
+    @IBOutlet weak var leftPlayerSelection: UIStackView!
+    @IBOutlet weak var rightPlayerSelection: UIStackView!
+    
+    @IBOutlet weak var leftHpLbl: UILabel!
+    @IBOutlet weak var rightHpLbl: UILabel!
+    
+    
+    @IBOutlet weak var leftSoldier: UIImageView!
+    @IBOutlet weak var leftOrc: UIImageView!
+    @IBOutlet weak var rightOrc: UIImageView!
+    @IBOutlet weak var rightSoldier: UIImageView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+
+    @IBAction func onPressedLeftAttackBtn(sender: AnyObject) {
+    }
+    
+    @IBAction func onPressedRightAttackBtn(sender: AnyObject) {
+    }
+    
+    @IBAction func onPressedLeftOrcBtn(sender: AnyObject) {
+    }
+    
+    @IBAction func onPressedLeftSoldierBtn(sender: AnyObject) {
     }
 
-
+    @IBAction func onPressedRightOrcBtn(sender: AnyObject) {
+    }
+    
+    @IBAction func onPressedRightSoldierBtn(sender: AnyObject) {
+    }
+    
 }
 
